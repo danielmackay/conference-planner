f104ff62aa85212465d138f3abbcccc15031925f
Daniel Mackay <daniel.mackay@levo.com.au>
2021-02-28 20:59:47 +1000

    Added speaker entity

--- GraphQL/Data/Speaker.cs
+++ GraphQL/Data/Speaker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,16 @@ namespace ConferencePlanner.GraphQL.Data
 {
     public class Speaker
     {
+        public int Id { get; set; }
 
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(4000)]
+        public string Bio { get; set; }
+
+        [StringLength(1000)]
+        public virtual string WebSite { get; set; }
     }
 }
