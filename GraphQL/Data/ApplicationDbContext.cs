81d89fc1b550fd95dc288a68d09b96b5161229f0
Daniel Mackay <daniel.mackay@levo.com.au>
2021-02-28 20:54:31 +1000

    Add project files.

---
+++ GraphQL/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferencePlanner.GraphQL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
