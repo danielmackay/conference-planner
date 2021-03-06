6b331380d1782df60e87a21fa87446a274c75e92
Daniel Mackay <daniel.mackay@levo.com.au>
2021-03-06 15:26:27 +1000

    Added UseUpperCaseAttribute.

---
+++ GraphQL/Extensions/UseUpperCaseAttribute.cs
@@ -0,0 +1,16 @@
+﻿
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+
+using System.Reflection;
+
+namespace ConferencePlanner.GraphQL.Extensions
+{
+    public class UseUpperCaseAttribute : ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(IDescriptorContext context, IObjectFieldDescriptor descriptor, MemberInfo member)
+        {
+            descriptor.UseUpperCase();
+        }
+    }
+}
